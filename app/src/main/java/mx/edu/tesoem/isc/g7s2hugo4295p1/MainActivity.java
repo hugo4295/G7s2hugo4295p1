2034a75648c9b7777c776d48b8f2c2ae3c9a9304
Victor Hugo De la O Martínez <hugo_4295@hotmail.com>
2021-10-18 21:25:59 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/g7s2hugo4295p1/MainActivity.java
@@ -0,0 +1,16 @@
+package mx.edu.tesoem.isc.g7s2hugo4295p1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
